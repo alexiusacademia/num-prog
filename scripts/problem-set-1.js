4cd3fd0eb2781c375eaf11be7a1c4660baafe918
syncster31 <alexius.academia@gmail.com>
2017-01-06 14:24:53 +0800

    -

--- scripts/problem-set-1.js
+++ scripts/problem-set-1.js
@@ -1,1 +1,8 @@
-var myApp = angular.module("problemSet", []);
+/// <reference path="angular.js" />
+var problemSetApp = angular.module("problemSetModule", []);
+
+var problemSetController = function($scope) {
+	$scope.title = "Numerical Methods - Problem Set # 1";
+};
+
+problemSetApp.controller("problemSetController", problemSetController);
